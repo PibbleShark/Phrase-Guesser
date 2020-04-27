ebed29872f9cfb4f2d552537c8556493acf44719
Joey Wardenaar <wardenj@gmail.com>
2020-04-27 16:51:19 -0700

    underscore string
    created a new string from the original that replaces the character with underscores

--- character.py
+++ character.py
@@ -1,5 +1,15 @@
-from phrase import PhraseGenerator
 
 
-class Character(PhraseGenerator):
-    
+class Character:
+
+    def __init__(self, string):
+        self.split_string = string.split(' ')
+        self.string = string
+
+    def display_length(self):
+        return ' '.join(['_'*len(word) for word in self.split_string])
+        
+
+
+
+        
